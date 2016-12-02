254da486e117254f2bf2f907a7e2287364ef99f1
mposa <mposa@mit.edu>
2016-12-02 13:47:18 -0500

    Added a timeout to CommandDispatch.

--- Acrobot_setup/CommandDisbatch/CommandDisbatch.cpp
+++ Acrobot_setup/CommandDisbatch/CommandDisbatch.cpp
@@ -18,15 +18,14 @@ lcm_t * lcm = NULL;
 lcmt_acrobot_u newest_command;
 lcmt_acrobot_u previous_command;
 lcmt_motor_command motor_command;
+long int last_time_ms;
 
 /*
 Go through all the command signals and only publish to the motor controller when the command for its motor has changed
 */
 void lcm_publish_control(void) 
 {
-	if(newest_command.tau != previous_command.tau)
-		lcmt_motor_command_publish(lcm,"acrobot_in", &motor_command);
-
+  lcmt_motor_command_publish(lcm,"acrobot_in", &motor_command);
 	memcpy(&previous_command,&newest_command,sizeof(lcmt_acrobot_u));
 }
 
@@ -50,8 +49,6 @@ void translate_control(void)
 	motor_command.command = newest_command.tau;
 
 	motor_command.timestamp = newest_command.timestamp;
-
-	lcm_publish_control();
 }
 
 void lcm_command_handler(const lcm_recv_buf_t *rbuf, const char* channel, const lcmt_acrobot_u *msg, void *user)
@@ -59,6 +56,10 @@ void lcm_command_handler(const lcm_recv_buf_t *rbuf, const char* channel, const 
 	memcpy(&newest_command,msg,sizeof(lcmt_acrobot_u));
 	translate_control();
 	lcm_publish_control();
+
+  struct timeval time;
+  gettimeofday(&time, NULL);
+  last_time_ms = time.tv_sec*1000 + time.tv_usec/1000;
 }
 
 int main(int argc, char** argv)
@@ -66,15 +67,27 @@ int main(int argc, char** argv)
 	//Set up LCM
 	lcm = lcm_create("udpm://239.255.76.67:7667?ttl=0");
 	if (!lcm) return 1;
-
 	// subscribe to relevant channels:
 	lcmt_acrobot_u_subscription_t * command_sub = lcmt_acrobot_u_subscribe (lcm, "acrobot_u", &lcm_command_handler, NULL);
-
 	Initialize();
-
+  
+  int timeout_ms = 500;
+  struct timeval time;
+  gettimeofday(&time, NULL);
+  last_time_ms = time.tv_sec*1000 + time.tv_usec/1000;
+  long int time_ms;
 	while (1)
 	{
-		lcm_handle(lcm);
+    gettimeofday(&time, NULL);
+    time_ms = time.tv_sec*1000 + time.tv_usec/1000;
+    if (time_ms-last_time_ms > timeout_ms) {
+      // timeout! publish a zero control message
+      last_time_ms = time_ms;
+      newest_command.tau = 0;
+      newest_command.timestamp = time_ms;
+      lcm_publish_control();
+    }
+		lcm_handle_timeout(lcm,50);
 	}
 
 	lcmt_acrobot_u_unsubscribe (lcm, command_sub);
