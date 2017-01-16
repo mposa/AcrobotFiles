db593b752f408a0ce14b89a1a71737b788d93ddc
mposa <mposa@mit.edu>
2017-01-16 14:04:14 -0500

    An older sysid commit.

--- SystemIdentification/createIDDataFromLog.m
+++ SystemIdentification/createIDDataFromLog.m
@@ -1,41 +1,73 @@
-function [sysdata,x0, data] = createIDDataFromLog(filename,start,finish)
-  % python conversion
-  setenv('PYTHONPATH',strcat(pwd,'/../LCMTypes/'));
-  oldfile = strcat('../AcrobotLogs/',filename);
-  newfile = strcat('../AcrobotLogs/',filename,'.mod');
-  system(['python ../AcrobotLogs/lcm_log_timestamps.py ' oldfile ' ' newfile ' acrobot_types']);
+function [sysdata,x0] = createIDDataFromLog(filename,start,finish,trimStart,maxT)
 
-  channels = {'acrobot_y','acrobot_xhat','acrobot_u','acrobot_out'};
-  coders = {AcrobotYCoder(),AcrobotStateCoder(),AcrobotInputCoder(),AcrobotOutCoder()};
-  data = readLog(newfile,channels,coders,start,finish);
-  
-  dt = .005;
-  t = unique(data{1}.t);
-  t = t(1):dt:t(end);
-  
-  [~,Ix] = unique(data{2}.t);
-  x = data{2}.data(:,Ix);
-  
-  [~,Iu] = unique(data{3}.t);
-  u = data{3}.data(:,Iu);
-  
-  [~,Iy] = unique(data{1}.t);
-  y = data{1}.data(:,Iy);
-  
-  
-  y = interp1(data{1}.t(Iy),y',t,'linear','extrap')';
-  x = interp1(data{2}.t(Ix),x',t,'linear','extrap')';
-  u = interp1(data{3}.t(Iu),u,t,'linear','extrap');
-  
-  
-  y(1:2,:) = y(1:2,:) + repmat(x(1:2,1) - y(1:2,1),1,length(t));
-  
-  t = t-t(1);
-  
-  outputs = y(1:2,:);
+if nargin < 4
+  trimStart = false;
+end
+if nargin < 5
+  maxT = inf;
+end
+
+% python conversion
+setenv('PYTHONPATH',strcat(pwd,'/../LCMTypes/'));
+oldfile = strcat('../AcrobotLogs/',filename);
+newfile = strcat('../AcrobotLogs/',filename,'.mod');
+system(['python ../lcm_log_timestamps.py ' oldfile ' ' newfile ' acrobot_types']);
+
+channels = {'acrobot_y','acrobot_xhat','acrobot_u','acrobot_out'};
+coders = {AcrobotYCoder(),AcrobotStateCoder(),AcrobotInputCoder(),AcrobotOutCoder()};
+data = readLog(newfile,channels,coders,start,finish);
+
+dt = .005;
+t = unique(data{1}.t);
+t = t(1):dt:t(end);
+
+[~,Ix] = unique(data{2}.t);
+x = data{2}.data(:,Ix);
+
+[~,Iu] = unique(data{3}.t);
+u = data{3}.data(:,Iu);
+
+[~,Iy] = unique(data{1}.t);
+y = data{1}.data(:,Iy);
+
+
+y = interp1(data{1}.t(Iy),y',t,'linear','extrap')';
+x = interp1(data{2}.t(Ix),x',t,'linear','extrap')';
+u = interp1(data{3}.t(Iu),u,t,'linear','extrap');
+
+if(trimStart)
+  i0 = find(y(3,:),1);
+  t = t(i0:end);
+  y = y(:,i0:end);
+  x = x(:,i0:end);
+  u = u(:,i0:end);
+end
+
+
+y(1:2,:) = y(1:2,:) + repmat(x(1:2,1) - y(1:2,1),1,length(t));
+
+t = t-t(1);
+
+outputs = y(1:2,:);
 %   inputs = u;
-  inputs = y(3,:);
+inputs = y(3,:);
+
+if maxT < inf
+  maxN = ceil(maxT/dt);
+  i0 = 1;
+  i1 = min(length(t),i0+maxN-1);
   
-  sysdata = iddata(outputs',inputs',dt);
-  x0 = x(:,1);
+  sysdata = {};
+  x0 = {};
+  
+  while(i0 < length(t))
+    sysdata{end+1} = iddata(outputs(:,i0:i1)',inputs(:,i0:i1)',dt);
+    x0{end+1} = x(:,i0);    
+    i0 = i1+1;
+    i1 = min(length(t),i0+maxN-1);
+  end  
+else
+  sysdata = {iddata(outputs',inputs',dt)};
+  x0 = {x(:,1)};
+end
 end
--- SystemIdentification/runSysID.m
+++ SystemIdentification/runSysID.m
@@ -11,15 +11,27 @@ javaaddpath('../LCMTypes/acrobot_types.jar')
 % % finish = [1;1;.3;1];
 % finish = [.5;1;.3;1];
 
-filenames = {'12-07-2016/sine_1Hz_6.log',...
-  '12-07-2016/sine_1Hz_3.log',...
-  '12-07-2016/sine_2Hz_6.log',...
-  '12-07-2016/sine_0_5Hz_6.log',...
-  '12-07-2016/sine_0_5Hz_3.log'};
+% filenames = {'12-07-2016/sine_1Hz_6.log',...
+%   '12-07-2016/sine_1Hz_3.log',...
+%   '12-07-2016/sine_2Hz_6.log',...
+%   '12-07-2016/sine_0_5Hz_6.log',...
+%   '12-07-2016/sine_0_5Hz_3.log'};
+%   %'12-07-2016/sine_2Hz_3.log',...
+
+% filenames = {'12-12-2016/6_sin_2_pi',...
+%   '12-12-2016/6_sin_4_pi',...
+%   '12-12-2016/6_sin_pi',...
+%   '12-12-2016/6_sin_1_5_pi',...
+%   '12-12-2016/6_sin_0_5_pi'};
+
+filenames = {'12-12-2016/6_sin_pi','12-14-2016/swingup2.log'};
+start = [0;.57];
+finish = [1;.8];
+
   %'12-07-2016/sine_2Hz_3.log',...
 
-start = [.33, .2, .45, .42, .36]; %.45,
-finish = [.7, .75, .9, .88, .85]; % .85, 
+% start = [.33, .2, .45, .42, .36]; %.45,
+% finish = [.7, .75, .9, .88, .85]; % .85, 
 
 
 % filenames = {'12-07-2016/sine_1Hz_6.log',...
@@ -42,12 +54,15 @@ sysiddata = {};
 x0 = {};
 data = {};
 for i=1:length(filenames),
+  [sysiddatai,x0i] = createIDDataFromLog(filenames{i},start(i),finish(i),true,1);
+  sysiddata = [sysiddata,sysiddatai];
+  x0 = [x0,x0i];
 %   file_times = start(i):.02:finish(i);
-  file_times = [start(i):.02:finish(i)];
+%   file_times = [start(i):.02:finish(i)];
 % file_times = [start(i) finish(i)];
-  for j=1:length(file_times)-1,
-    [sysiddata{end+1},x0{end+1},data{end+1}] = createIDDataFromLog(filenames{i},file_times(j),file_times(j+1));
-  end
+%   for j=1:length(file_times)-1,
+%     [sysiddata{end+1},x0{end+1},data{end+1}] = createIDDataFromLog(filenames{i},file_times(j),file_times(j+1));
+%   end
 end
 
 data_bkp = sysiddata;
@@ -59,13 +74,13 @@ x0 = x0_bkp;
 
 
 % 
-% sysiddata = sysiddata(1);
-% x0 = x0(1);
-for i=1:length(sysiddata)
-%   maxv(i) = min(max(abs(data{i}{2}.data(3:4.,:))'));
-%   meanu(i) = mean(abs(data{i}{3}.data));
-  maxdt(i) = max(diff(data{i}{3}.t));
-end
+% % sysiddata = sysiddata(1);
+% % x0 = x0(1);
+% for i=1:length(sysiddata)
+% %   maxv(i) = min(max(abs(data{i}{2}.data(3:4.,:))'));
+% %   meanu(i) = mean(abs(data{i}{3}.data));
+%   maxdt(i) = max(diff(data{i}{3}.t));
+% end
 
 
 %%
@@ -85,9 +100,9 @@ end
 
 % I = 1:length(sysiddata);
 % I = setdiff(I, [3 4 8 12 13 16 23]);
-I = find(maxdt < .01);
-sysiddata=sysiddata(I);
-x0=x0(I);
+% I = find(maxdt < .01);
+% sysiddata=sysiddata(I);
+% x0=x0(I);
 
 %%
 z = merge(sysiddata{:});
@@ -139,7 +154,10 @@ InitParams(10).Minimum = -inf;
 % Parameters =     [    2.2244    0.5508    0.5134    0.8039    0.8362    0.2078    0.0390    0.8999    0.2293 0];
 
 % from 12/8
- Parameters =  [2.3865    0.3743    0.5690    1.3408    2.0589    0.0547    0.0288    1.6038    0.4172   -0.0243*0];
+%  Parameters =  [2.3865    0.3743    0.5690    1.3408    2.0589    0.0547    0.0288    1.6038    0.4172   -0.0243*0];
+ 
+ % From 12/12
+ Parameters = [    2.4367    0.6178    0.5263    1.6738    1.5651    0.0320    0.0413    2.0824    0.5065    0.0011];
   
 for i=1:length(InitParams),
   InitParams(i).Value = Parameters(i);
@@ -175,7 +193,7 @@ end
 
 nlgr = idnlgrey(FileName, Order, InitParams, InitialStates, Ts);
 %%
-nlgr = pem(z, nlgr, 'Display', 'Full','MaxIter',5);
+nlgr = pem(z, nlgr, 'Display', 'Full','MaxIter',100);
 
 % figure;
 % compare(getexp(z,1), nlgr);
@@ -192,12 +210,13 @@ p = nlgr.Report.Parameters;
 
 % 8,12, 39, 41, 44, 45, 49, 51, 53, 55, 80
 %%
-i = 1
+i = i+1;
+t_offset = .0;
 plant = AcrobotPlantSmooth(p.ParVector);
 % plant = AcrobotPlantSmooth();
 experiment = getexp(z,i);
 t_exp = experiment.Ts*(0:length(experiment.InputData)-1);
-utraj = PPTrajectory(foh(t_exp,experiment.InputData'));
+utraj = PPTrajectory(foh([-t_offset t_exp]+t_offset,[0 experiment.InputData']));
 utraj = utraj.setOutputFrame(plant.getInputFrame);
 plant_ol = utraj.cascade(plant);
 % traj = plant_ol.simulate([0 t_exp(end)],x0{i});
@@ -207,6 +226,7 @@ x_exp = traj.eval(t_exp);
 figure(2)
 subplot(2,1,1)
 plot(t_exp,experiment.OutputData(:,1),t_exp,x_exp(1,:))
+title(i)
 subplot(2,1,2)
 plot(t_exp,experiment.OutputData(:,2),t_exp,x_exp(2,:))
 legend('exp','fit')
